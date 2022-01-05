6782094f4da7197e80663e564cedaa24cbbcb0c1
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-01-05 22:02:48 +0100

    faicons

--- src/pages/landingpage/Landingpage.js
+++ src/pages/landingpage/Landingpage.js
@@ -2,8 +2,12 @@ import HeroImage from "../../assets/grsDRXc.png";
 import AboutImage from "../../assets/afzwKAO.png";
 import TokePro from "../../assets/toke pro.gif";
 import TokeToke from "../../assets/toke-toke.gif";
-import Bud from "../../assets/xQN2bV9.jpg"
+import Bud from "../../assets/xQN2bV9.jpg";
+import DankMonk from "../../assets/mWNcDgt.png";
 import Navbar from "../../components/navigation/Navbar";
+import Metaverse from "../../assets/Wg86pOz.png";
+import { BsDiscord } from "react-icons/bs";
+import { FaTwitter } from "react-icons/fa";
 import "./Landingpage.css";
 
 function Landingpage() {
@@ -14,6 +18,8 @@ function Landingpage() {
         <div className="page_body_left">
           <h1 className="pb-2">SOLWEEDPUNKZ</h1>
           <h2 className="toke_toke">TOKE TOKE...</h2>
+          <BsDiscord size={60} />
+          <FaTwitter size={60} />
         </div>
         <div className="page_body_right">
           <img src={TokePro} alt="hero_image" />
@@ -50,7 +56,7 @@ function Landingpage() {
       {/* Toke Toke Arts */}
       <div className="sol_toke_arts container mt-4">
         <div className="toke_img_title">
-          <h2>SOLWEEDPUNKZ ARTS</h2>
+          <h2>SOLWEEDPUNKZ ARTS...</h2>
         </div>
         <div className="toke_toke_img">
           <img src={TokeToke} alt="" />
@@ -61,10 +67,39 @@ function Landingpage() {
       {/* The team */}
       <div className="solweedpunk_team container mt-4">
         <div className="solweedpunk_team_title">
-          <h2>SOLWEEDPUNKZ TEAM</h2>
+          <h2>SOLWEEDPUNKZ TEAM...</h2>
         </div>
         <div className="solweedpunk_team_details">
-
+          <div className="team">
+            <div className="team_img">
+              <img src={Bud} alt="moon" />
+            </div>
+            <div className="team_name">
+              <h3>Moonbox</h3>
+              <p>Artist and Founder of all things Solweedpunk</p>
+            </div>
+          </div>
+          <div className="team">
+            <div className="team_img">
+              <img src={DankMonk} alt="dank monk" />
+            </div>
+            <div className="team_name">
+              <h3>DankMonke</h3>
+              <p>Helper monke, Investor, Advisor, Mod, Stuff doer</p>
+            </div>
+          </div>
+          <div className="team">
+            <div className="team_img">
+              <img src={Metaverse} alt="meta" />
+            </div>
+            <div className="team_name">
+              <h3>Metaversol</h3>
+              <p>
+                Formerly Test Guys - They are the big brains behind our
+                metaverse world
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
