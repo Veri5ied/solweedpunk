592547b8d56a917caeb4b65e21e67b05032849ec
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-01-08 15:43:11 +0100

    made team reusable

--- src/pages/team/Team.js
+++ src/pages/team/Team.js
@@ -1,86 +1,69 @@
-import DankMonk from "../../assets/mWNcDgt.png";
-import Terp from "../../assets/terp.png";
-import Alvin from "../../assets/alvin.png";
-import Nico from "../../assets/nico.jpg";
-import Metaverse from "../../assets/metaversol.png";
-import Bud from "../../assets/xQN2bV9.jpg";
 import { FaTwitter } from "react-icons/fa";
-
-import "./Team.css"
+import Moon from "../../assets/xQN2bV9.jpg";
+import Dank from "../../assets/mWNcDgt.png";
+import Meta from "../../assets/metaversol.png";
+import Terp from "../../assets/terp.png";
+import Nico from "../../assets/nico.jpg";
+import Alvin from "../../assets/alvin.png";
+import "./Team.css";
 
 function Team() {
+  const team = [
+    {
+      name: "Moonbox",
+      role: "Artist and Founder of all things on Solweedpunkz",
+      social: "",
+      image: `${Moon}`,
+    },
+    {
+      name: "DankMonke",
+      role: "Helper monke, Investor, Advisor, Mod, Stuff doer",
+      social: "",
+      image: `${Dank}`,
+    },
+    {
+      name: "Metaversol",
+      role: "Formerly Test Guys - They are the big brains behind our metaverse world",
+      social: "",
+      image: `${Meta}`,
+    },
+    {
+      name: "Terp Doctor",
+      role: "Developer, community builder and moderator.",
+      social: "",
+      image: `${Terp}`,
+    },
+    {
+      name: "Nico GT",
+      role: "Community manager and 3D artist and moderator.",
+      social: "",
+      image: `${Nico}`,
+    },
+    {
+      name: "Veri5ied",
+      role: "Software Engineer, building and making the web accessible to many",
+      social: "",
+      image: `${Alvin}`,
+    },
+  ];
   return (
     <>
       <div className="solweedpunk_team_title">
         <h2>SOLWEEDPUNKZ TEAM...</h2>
       </div>
       <div className="solweedpunk_team_details">
-        <div className="team">
-          <div className="team_img">
-            <img src={Bud} alt="moon" />
+        {team.map((tm) => (
+          <div className="team" key={tm.id}>
+            <div className="team_img">
+              <img src={tm.image} alt="team_image" />
+            </div>
+            <div className="team_name">
+              <h3>{tm.name}</h3>
+              <p>{tm.role}</p>
+              <FaTwitter size={26} />
+            </div>
           </div>
-          <div className="team_name">
-            <h3>Moonbox</h3>
-            <p>Artist and Founder of all things on Solweedpunk</p>
-            <FaTwitter size={26} />
-          </div>
-        </div>
-        <div className="team">
-          <div className="team_img">
-            <img src={DankMonk} alt="dank monk" />
-          </div>
-          <div className="team_name">
-            <h3>DankMonke</h3>
-            <p>Helper monke, Investor, Advisor, Mod, Stuff doer</p>
-            <FaTwitter size={26} />
-          </div>
-        </div>
-        <div className="team">
-          <div className="team_img">
-            <img src={Metaverse} alt="meta" />
-          </div>
-          <div className="team_name">
-            <h3>Metaversol</h3>
-            <p>
-              Formerly Test Guys - They are the big brains behind our metaverse
-              world
-            </p>
-            <FaTwitter size={26} />
-          </div>
-        </div>
-        {/* Team two */}
-        <div className="team">
-          <div className="team_img">
-            <img src={Terp} alt="terp" />
-          </div>
-          <div className="team_name">
-            <h3>Terp Doctor</h3>
-            <p>Developer, community builder and moderator.</p>
-            <FaTwitter size={26} />
-          </div>
-        </div>
-        <div className="team">
-          <div className="team_img">
-            <img src={Nico} alt="nico" />
-          </div>
-          <div className="team_name">
-            <h3>Nico GT</h3>
-            <p>Community manager and 3D artist and moderator.</p>
-            <FaTwitter size={26} />
-          </div>
-        </div>
-        <div className="team">
-          <div className="team_img">
-            <img src={Alvin} alt="alvin" />
-          </div>
-          <div className="team_name">
-            <h3>Veri5ied</h3>
-            <p>
-              Software Engineer, building and making the web accessible to many
-            </p>
-            <FaTwitter size={26} />
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
